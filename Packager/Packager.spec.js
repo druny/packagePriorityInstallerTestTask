2ce8876bc25bf372baba1502092e3fd9dc9b0b55
Andrew Danilenko <druny195@gmail.com>
2019-07-02 01:14:27 +0300

    Refactore Packager.spec

--- Packager/Packager.spec.js
+++ Packager/Packager.spec.js
@@ -10,38 +10,44 @@ const {
 
 const SUCCEED_RESULT_COLOR = '\x1b[36m%s\x1b[0m';
 
+const getSucceedLog =
+  (methodName) => console.log(SUCCEED_RESULT_COLOR, `[${methodName}]`, 'DONE');
+
 const PackagerSpec = {
   wrongInput() {
+    const methodName = this.wrongInput.name;
     const mockedWrongData = '';
     const PackagerBuilder = new Packager(mockedWrongData);
 
     try {
       PackagerBuilder.exec();
 
-      throw new Error('[wrongInput] got wrong input');
+      throw new Error(`[${methodName}] got wrong input`);
     } catch ({ message }) {
       assert.deepEqual(message, WRONG_INPUT_DATA_ERROR_MESSAGE);
 
-      console.log(SUCCEED_RESULT_COLOR, '[wrongInput]', 'DONE')
+      getSucceedLog(methodName);
     }
   },
 
   hasNotParsedData() {
+    const methodName = this.hasNotParsedData.name;
     const mockedWrongData = '';
     const PackagerBuilder = new Packager(mockedWrongData);
 
     try {
       PackagerBuilder.buildFlow();
 
-      throw new Error('[hasNotParsedData] data has not parsed');
+      throw new Error(`[${methodName}] data has not parsed`);
     } catch ({ message }) {
       assert.deepEqual(message, HAS_NOT_PARSED_DATA);
 
-      console.log(SUCCEED_RESULT_COLOR, '[hasNotParsedData]', 'DONE')
+      getSucceedLog(methodName);
     }
   },
 
   basicCall() {
+    const methodName = this.basicCall.name;
     const mockedBasicData = PackagesRepository.getPackages();
     const mockedResult = 'KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream';
 
@@ -49,21 +55,23 @@ const PackagerSpec = {
 
     assert.deepEqual(PackagerBuilder.exec(), mockedResult);
 
-    console.log(SUCCEED_RESULT_COLOR, '[basicCall]', 'DONE');
+    getSucceedLog(methodName);
   },
 
   cyclicDependenciesCall() {
+    const methodName = this.cyclicDependenciesCall.name;
+
     const mockedCycleDependenciesData = PackagesRepository.getCycleDependenciesPackages();
     const PackagerBuilder = new Packager(mockedCycleDependenciesData);
 
     try {
       PackagerBuilder.exec();
 
-      throw new Error('[cyclicDependenciesCall] was not determined cyclic dependency');
+      throw new Error(`[${methodName}] was not determined cyclic dependency`);
     } catch ({ message }) {
       assert.deepEqual(message, DEPENDENCIES_CYCLE_ERROR_MESSAGE);
 
-      console.log(SUCCEED_RESULT_COLOR, '[cyclicDependenciesCall]', 'DONE')
+      getSucceedLog(methodName);
     }
   },
 
