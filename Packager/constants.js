b1f794c4f23e06d8e1a7f60498079d8eb55c944e
Andrew Danilenko <druny195@gmail.com>
2019-07-02 00:44:56 +0300

    Add pacakger constants

---
+++ Packager/constants.js
@@ -0,0 +1,7 @@
+const DEPENDENCIES_CYCLE_ERROR_MESSAGE = 'Input contains a cycle';
+const WRONG_INPUT_DATA_ERROR_MESSAGE = 'Sent wrong packages input data';
+
+module.exports = {
+  DEPENDENCIES_CYCLE_ERROR_MESSAGE,
+  WRONG_INPUT_DATA_ERROR_MESSAGE
+};
