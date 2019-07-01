050e71688940cffb919285b941ed41925fe2c39f
Andrew Danilenko <druny195@gmail.com>
2019-07-02 01:31:25 +0300

    Add comment to packager repository

--- repository/packages.js
+++ repository/packages.js
@@ -1,9 +1,14 @@
 const mockedPackages = require('./mockedPackages');
 
+/**
+ * Because of it's test task
+ * I neglected that repository uses for test purpose
+* */
 const Packages = {
   getPackages() {
     return mockedPackages.dependencies;
   },
+
   getCycleDependenciesPackages() {
     return mockedPackages.cycleDependencies;
   }
