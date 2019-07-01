2477f21b5179491a9da3df924aab98300c72e326
Andrew Danilenko <druny195@gmail.com>
2019-07-02 01:04:31 +0300

    Add HAS_NOT_PARSED_DATA packager constant

--- Packager/constants.js
+++ Packager/constants.js
@@ -1,7 +1,9 @@
+const HAS_NOT_PARSED_DATA = 'Package dependencies has not parsed';
 const DEPENDENCIES_CYCLE_ERROR_MESSAGE = 'Input contains a cycle';
 const WRONG_INPUT_DATA_ERROR_MESSAGE = 'Sent wrong packages input data';
 
 module.exports = {
+  HAS_NOT_PARSED_DATA,
   DEPENDENCIES_CYCLE_ERROR_MESSAGE,
   WRONG_INPUT_DATA_ERROR_MESSAGE
 };
